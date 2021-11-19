248096c904b27ec8a78f4571a53bd9d7c8784427
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-11-19 17:54:05 +0100

    Create a IoC container

--- src/index.php
+++ src/index.php
@@ -6,17 +6,24 @@ require __DIR__ . '/../vendor/autoload.php';
 
 require 'database/database.php'; // TODO: autoload
 
+use KarmekK\Mcat\Database\Database;
 use KarmekK\Mcat\Database\SqliteDatabase;
+use League\Container\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+$container = new Container();
+$container->add(Database::class, SqliteDatabase::class);
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $req, Response $res) {
-    $db = new SqliteDatabase();
+    /** @var Database */
+    $db = $this->get(Database::class);
 
     foreach ($db->getPdo()->query('SELECT * FROM tracks') as $track) {
         var_dump($track);
@@ -28,8 +35,8 @@ $app->get('/', function (Request $req, Response $res) {
 });
 
 $app->get('/seed', function (Request $req, Response $res) {
-    // I'll remove duplicated code
-    $db = new SqliteDatabase();
+    /** @var Database */
+    $db = $this->get(Database::class);
 
     $db->getPdo()->exec('INSERT INTO tracks (title) VALUES ("hi")');
 
