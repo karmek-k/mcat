20731b2cde38b1289e1970d22d2ef8411c84d266
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-12-13 19:35:44 +0100

    Set up webpack for watching for changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const root = path.join(__dirname, 'frontend');
+const buildDir = path.join(root, 'build');
 
 module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: path.join(root, 'js', 'index.js'),
   output: {
     filename: 'script.js',
-    path: path.join(root, 'build')
+    path: buildDir
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(root, 'templates', 'index.html')
     })
-  ]
+  ],
+
+  // TODO: change before production build
+  devtool: 'inline-source-map'
 };
