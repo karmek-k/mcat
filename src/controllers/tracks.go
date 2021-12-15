70052a9a604e1043cfcbbafd9f848200ba7e01e8
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-12-15 17:47:59 +0100

    Implement Track list and details controllers

--- src/controllers/tracks.go
+++ src/controllers/tracks.go
@@ -4,17 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/karmek-k/mcat/src/db"
+	"github.com/karmek-k/mcat/src/models"
 )
 
 func TrackList(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"mysia": "chomik",
-	})
+	var tracks []models.Track
+
+	db.DB.Find(&tracks)
+
+	c.JSON(http.StatusOK, tracks)
 }
 
 func TrackDetails(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"catIndex": c.Param("id"),
-	})
+	var track models.Track
+
+	result := db.DB.First(&track, c.Param("id"))
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, track)
 }
 
