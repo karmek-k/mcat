a9590b08313bff17e8f4ea55d8761e73c51235ab
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-12-15 18:36:36 +0100

    Add a seeding utility

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"embed"
+	"log"
+	"os"
 
 	"github.com/karmek-k/mcat/src/router"
+	"github.com/karmek-k/mcat/utils"
 )
 
 //go:embed frontend/build
 var publicFiles embed.FS
 
 func main() {
+	if os.Getenv("SEED") == "1" {
+		utils.SeedDB()
+		log.Println("seeding complete")
+
+		return
+	}
+
 	r := router.SetupRouter(&publicFiles)
 	
 	r.Run(":8000")
